76bbae70d56a15303679edbc6d8d534ddcb5ce1f
David Shelton <110427844+DavidSRepo@users.noreply.github.com>
2023-02-24 06:18:42 -0500

    Add files via upload

---
+++ abcStudentViewer2/Form1.Designer.vb
@@ -0,0 +1,281 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblRecordNum = New System.Windows.Forms.Label()
+        Me.lblFName = New System.Windows.Forms.Label()
+        Me.lblLName = New System.Windows.Forms.Label()
+        Me.lblClass = New System.Windows.Forms.Label()
+        Me.lblGPA = New System.Windows.Forms.Label()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnSort = New System.Windows.Forms.Button()
+        Me.btnSwap = New System.Windows.Forms.Button()
+        Me.lblB = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnPrev = New System.Windows.Forms.Button()
+        Me.HScrollBar2 = New System.Windows.Forms.HScrollBar()
+        Me.HScrollBar1 = New System.Windows.Forms.HScrollBar()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblRecordNum
+        '
+        Me.lblRecordNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblRecordNum.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRecordNum.Location = New System.Drawing.Point(131, 17)
+        Me.lblRecordNum.Name = "lblRecordNum"
+        Me.lblRecordNum.Size = New System.Drawing.Size(46, 27)
+        Me.lblRecordNum.TabIndex = 0
+        '
+        'lblFName
+        '
+        Me.lblFName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFName.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFName.Location = New System.Drawing.Point(131, 57)
+        Me.lblFName.Name = "lblFName"
+        Me.lblFName.Size = New System.Drawing.Size(90, 27)
+        Me.lblFName.TabIndex = 1
+        '
+        'lblLName
+        '
+        Me.lblLName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblLName.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLName.Location = New System.Drawing.Point(131, 100)
+        Me.lblLName.Name = "lblLName"
+        Me.lblLName.Size = New System.Drawing.Size(90, 27)
+        Me.lblLName.TabIndex = 2
+        '
+        'lblClass
+        '
+        Me.lblClass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblClass.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblClass.Location = New System.Drawing.Point(131, 145)
+        Me.lblClass.Name = "lblClass"
+        Me.lblClass.Size = New System.Drawing.Size(141, 27)
+        Me.lblClass.TabIndex = 3
+        '
+        'lblGPA
+        '
+        Me.lblGPA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGPA.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGPA.Location = New System.Drawing.Point(131, 189)
+        Me.lblGPA.Name = "lblGPA"
+        Me.lblGPA.Size = New System.Drawing.Size(71, 27)
+        Me.lblGPA.TabIndex = 4
+        '
+        'btnNext
+        '
+        Me.btnNext.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNext.Location = New System.Drawing.Point(466, 12)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(118, 53)
+        Me.btnNext.TabIndex = 5
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(11, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 18)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Record Number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(10, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 18)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "First Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(10, 101)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 18)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Last Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(10, 146)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 18)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Class Level"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(10, 190)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 18)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "GPA"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.lblGPA)
+        Me.GroupBox1.Controls.Add(Me.lblClass)
+        Me.GroupBox1.Controls.Add(Me.lblLName)
+        Me.GroupBox1.Controls.Add(Me.lblFName)
+        Me.GroupBox1.Controls.Add(Me.lblRecordNum)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(302, 230)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Student Info"
+        '
+        'btnSort
+        '
+        Me.btnSort.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSort.Location = New System.Drawing.Point(331, 111)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(118, 53)
+        Me.btnSort.TabIndex = 12
+        Me.btnSort.Text = "Sort Array B"
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'btnSwap
+        '
+        Me.btnSwap.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSwap.Location = New System.Drawing.Point(332, 189)
+        Me.btnSwap.Name = "btnSwap"
+        Me.btnSwap.Size = New System.Drawing.Size(118, 53)
+        Me.btnSwap.TabIndex = 13
+        Me.btnSwap.Text = "Special Swap"
+        Me.btnSwap.UseVisualStyleBackColor = True
+        '
+        'lblB
+        '
+        Me.lblB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblB.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblB.Location = New System.Drawing.Point(455, 113)
+        Me.lblB.Name = "lblB"
+        Me.lblB.Size = New System.Drawing.Size(211, 36)
+        Me.lblB.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(486, 86)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(75, 20)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Array B( )"
+        '
+        'btnPrev
+        '
+        Me.btnPrev.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPrev.Location = New System.Drawing.Point(332, 13)
+        Me.btnPrev.Name = "btnPrev"
+        Me.btnPrev.Size = New System.Drawing.Size(118, 53)
+        Me.btnPrev.TabIndex = 16
+        Me.btnPrev.Text = "Previous"
+        Me.btnPrev.UseVisualStyleBackColor = True
+        '
+        'HScrollBar2
+        '
+        Me.HScrollBar2.Location = New System.Drawing.Point(36, 12)
+        Me.HScrollBar2.Name = "HScrollBar2"
+        Me.HScrollBar2.Size = New System.Drawing.Size(8, 8)
+        Me.HScrollBar2.TabIndex = 18
+        '
+        'HScrollBar1
+        '
+        Me.HScrollBar1.LargeChange = 1
+        Me.HScrollBar1.Location = New System.Drawing.Point(36, 262)
+        Me.HScrollBar1.Maximum = 7
+        Me.HScrollBar1.Minimum = 1
+        Me.HScrollBar1.Name = "HScrollBar1"
+        Me.HScrollBar1.Size = New System.Drawing.Size(157, 43)
+        Me.HScrollBar1.TabIndex = 19
+        Me.HScrollBar1.Value = 1
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnNext
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(689, 430)
+        Me.Controls.Add(Me.HScrollBar1)
+        Me.Controls.Add(Me.HScrollBar2)
+        Me.Controls.Add(Me.btnPrev)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblB)
+        Me.Controls.Add(Me.btnSwap)
+        Me.Controls.Add(Me.btnSort)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnNext)
+        Me.Name = "Form1"
+        Me.Text = "Student Viewer 2 (In-Class program)"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblRecordNum As Label
+    Friend WithEvents lblFName As Label
+    Friend WithEvents lblLName As Label
+    Friend WithEvents lblClass As Label
+    Friend WithEvents lblGPA As Label
+    Friend WithEvents btnNext As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnSort As Button
+    Friend WithEvents btnSwap As Button
+    Friend WithEvents lblB As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnPrev As Button
+    Friend WithEvents HScrollBar2 As HScrollBar
+    Friend WithEvents HScrollBar1 As HScrollBar
+End Class
---
+++ abcStudentViewer2/Form1.vb
@@ -0,0 +1,159 @@
+﻿
+' add your name here -- David Shelton
+' StudentViewer2
+' Date: 2/21/2020
+
+' Reads student data from a text file, stores it in an array of structures.
+' Shows one student at a time.
+' Will be updated with new tasks.
+
+Imports System.IO
+
+Public Class Form1
+
+    'Global variables
+    Dim i As Integer
+    Dim k As Integer
+    Dim numberOfStudents As Integer = 7    ' size of array
+    Dim student(numberOfStudents) As StudentRecord
+    Dim inputFile As StreamReader
+    Dim B(7) As Integer
+    Dim randbox As New Random
+
+    'Prepare the structure
+    Structure StudentRecord
+        Dim strFName As String
+        Dim strLName As String
+        Dim intClass As Integer
+        Dim sngGPA As Single
+    End Structure
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' no changes needed in this subroutine
+
+        ' it reads 7 students data from the file into the array
+        ' no error checks needed for this version
+
+        inputFile = File.OpenText("StudentData7.txt")
+
+        For k = 1 To numberOfStudents
+            student(k).strFName = inputFile.ReadLine()
+            student(k).strLName = inputFile.ReadLine()
+            student(k).intClass = inputFile.ReadLine()
+            student(k).sngGPA = inputFile.ReadLine()
+        Next k
+
+        inputFile.Close()
+
+        k = HScrollBar1.Value  ' set current student as 1 and show info
+        ShowAStudent(k)
+
+        For i = 1 To 7      ' creates array B with random values
+            B(i) = randbox.Next(20, 80)
+        Next
+        ShowArrayB()
+
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        ' no changes needed here (advances to next student)
+
+        If k < numberOfStudents Then  ' advance if not at end
+            k = k + 1
+        End If
+
+        ShowAStudent(k)     ' call function to show student k
+    End Sub
+
+    Private Sub btnPrev_Click(sender As Object, e As EventArgs) Handles btnPrev.Click
+        ' write code to back up one student (if not at beginning) by changing k
+        If k > 1 Then
+            k = k - 1
+        End If
+
+
+
+        ' do not change the next line, which shows student k
+        ShowAStudent(k)
+    End Sub
+
+    Private Sub ShowAStudent(HScrollbar)
+        ' shows one student's array data based on parameter k
+        lblRecordNum.Text = k
+        lblFName.Text = student(k).strFName
+        lblLName.Text = student(k).strLName
+        '  lblClass.Text = student(k).intClass
+        If student(k).intClass = 1 Then
+            lblClass.Text = " Freshman"
+        ElseIf student(k).intClass = 2 Then
+            lblClass.Text = "Sophmore"
+        Else
+            lblClass.Text = "Junior"
+        End If
+        lblGPA.Text = student(k).sngGPA
+
+    End Sub
+
+    Private Sub ShowArrayB()
+        ' call this routine to show array B
+        ' no changes needed
+        lblB.Text = " "
+        For i = 1 To 7
+            lblB.Text &= B(i) & "  "
+        Next
+    End Sub
+
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+        ' write the Exchange sort code to sort array B
+        Dim ReplacePosn As Integer
+        Dim Smallest As Integer
+        Dim SmallestPosn As Integer
+        Dim SwapHolder As Integer
+
+        For ReplacePosn = 1 To 6
+            Smallest = B(7)
+            SmallestPosn = 7
+            For k = (ReplacePosn - 1) To 7
+                If B(k) < B(ReplacePosn) Then
+                    Smallest = B(k)
+                    SmallestPosn = k
+                End If
+            Next k
+
+            SwapHolder = B(ReplacePosn)
+            B(ReplacePosn) = B(SmallestPosn)
+            B(SmallestPosn) = SwapHolder
+        Next ReplacePosn
+
+
+
+        ' do not change the next line, which shows the array B
+        ShowArrayB()
+    End Sub
+
+    Private Sub btnSwap_Click(sender As Object, e As EventArgs) Handles btnSwap.Click
+        'write code to swap all of student 3's info with all of student 4's info
+
+
+
+
+    End Sub
+
+    Private Sub HScrollBar1_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar1.Scroll
+        k = HScrollBar1.Value
+
+        lblRecordNum.Text = k
+        lblFName.Text = student(k).strFName
+        lblLName.Text = student(k).strLName
+        '  lblClass.Text = student(k).intClass
+        If student(k).intClass = 1 Then
+            lblClass.Text = " Freshman"
+        ElseIf student(k).intClass = 2 Then
+            lblClass.Text = "Sophmore"
+        Else
+            lblClass.Text = "Junior"
+        End If
+        lblGPA.Text = student(k).sngGPA
+
+    End Sub
+End Class
