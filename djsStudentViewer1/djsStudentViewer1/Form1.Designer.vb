281a3286c3aae665b2df33762ae63ea7d2ef29fe
David Shelton <110427844+DavidSRepo@users.noreply.github.com>
2023-02-24 06:12:35 -0500

    Add files via upload

---
+++ djsStudentViewer1/djsStudentViewer1/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblA = New System.Windows.Forms.Label()
+        Me.lblB = New System.Windows.Forms.Label()
+        Me.lblC = New System.Windows.Forms.Label()
+        Me.lblD = New System.Windows.Forms.Label()
+        Me.lblE = New System.Windows.Forms.Label()
+        Me.btnA = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblA
+        '
+        Me.lblA.AutoSize = True
+        Me.lblA.Location = New System.Drawing.Point(12, 18)
+        Me.lblA.Name = "lblA"
+        Me.lblA.Size = New System.Drawing.Size(44, 13)
+        Me.lblA.TabIndex = 0
+        Me.lblA.Text = "Number"
+        '
+        'lblB
+        '
+        Me.lblB.AutoSize = True
+        Me.lblB.Location = New System.Drawing.Point(167, 18)
+        Me.lblB.Name = "lblB"
+        Me.lblB.Size = New System.Drawing.Size(57, 13)
+        Me.lblB.TabIndex = 1
+        Me.lblB.Text = "First Name"
+        '
+        'lblC
+        '
+        Me.lblC.AutoSize = True
+        Me.lblC.Location = New System.Drawing.Point(300, 18)
+        Me.lblC.Name = "lblC"
+        Me.lblC.Size = New System.Drawing.Size(58, 13)
+        Me.lblC.TabIndex = 2
+        Me.lblC.Text = "Last Name"
+        '
+        'lblD
+        '
+        Me.lblD.AutoSize = True
+        Me.lblD.Location = New System.Drawing.Point(454, 18)
+        Me.lblD.Name = "lblD"
+        Me.lblD.Size = New System.Drawing.Size(61, 13)
+        Me.lblD.TabIndex = 3
+        Me.lblD.Text = "Class Level"
+        '
+        'lblE
+        '
+        Me.lblE.AutoSize = True
+        Me.lblE.Location = New System.Drawing.Point(614, 18)
+        Me.lblE.Name = "lblE"
+        Me.lblE.Size = New System.Drawing.Size(29, 13)
+        Me.lblE.TabIndex = 4
+        Me.lblE.Text = "GPA"
+        '
+        'btnA
+        '
+        Me.btnA.Location = New System.Drawing.Point(303, 136)
+        Me.btnA.Name = "btnA"
+        Me.btnA.Size = New System.Drawing.Size(89, 37)
+        Me.btnA.TabIndex = 5
+        Me.btnA.Text = "Click to show info"
+        Me.btnA.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnA)
+        Me.Controls.Add(Me.lblE)
+        Me.Controls.Add(Me.lblD)
+        Me.Controls.Add(Me.lblC)
+        Me.Controls.Add(Me.lblB)
+        Me.Controls.Add(Me.lblA)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblA As Label
+    Friend WithEvents lblB As Label
+    Friend WithEvents lblC As Label
+    Friend WithEvents lblD As Label
+    Friend WithEvents lblE As Label
+    Friend WithEvents btnA As Button
+End Class
---
+++ djsStudentViewer1/djsStudentViewer1/Form1.vb
@@ -0,0 +1,70 @@
+﻿' David Shelton
+' djsStudentViewer1
+'2/12/2020
+'Put 20 students information into an array, click a button to show the information one at a time
+'
+
+
+
+
+Imports System.IO
+
+Public Class Form1
+    Private Const strFILENAME As String = "StudentData.txt"
+
+    Structure StudentRecord
+        Public strFirstName As String
+        Public strLastName As String
+        Public intClassLevel As Integer
+        Public dblGPA As Double
+    End Structure
+
+    Dim Student(20) As StudentRecord 'array that holds student data record
+    Dim J As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim inputFile As StreamReader
+
+
+        If File.Exists(strFILENAME) = False Then
+            MessageBox.Show("File not found: " & strFILENAME, "Error")
+            Exit Sub
+        End If
+
+        inputFile = File.OpenText(strFILENAME)
+        'Putting the students into an array
+        For J = 1 To 20
+            Student(J).strFirstName = inputFile.ReadLine()
+            Student(J).strLastName = inputFile.ReadLine()
+            Student(J).intClassLevel = inputFile.ReadLine()
+            Student(J).dblGPA = inputFile.ReadLine()
+
+        Next J
+
+
+        'shows the first item
+        J = 1
+        lblA.Text = J.ToString
+        lblB.Text = Student(J).strFirstName
+        lblC.Text = Student(J).strLastName
+        lblD.Text = Student(J).intClassLevel
+        lblE.Text = Student(J).dblGPA
+
+
+
+    End Sub
+
+    Private Sub btnA_Click(sender As Object, e As EventArgs) Handles btnA.Click
+        'shows the stuff
+
+        If J < 20 Then
+            J = J + 1
+            lblA.Text = J.ToString
+            lblB.Text = Student(J).strFirstName
+            lblC.Text = Student(J).strLastName
+            lblD.Text = Student(J).intClassLevel
+            lblE.Text = Student(J).dblGPA
+        End If
+
+
+    End Sub
+End Class
