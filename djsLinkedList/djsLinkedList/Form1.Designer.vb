61470be1dd02f6574f0225a5a29c30eac7f8ff89
David Shelton <110427844+DavidSRepo@users.noreply.github.com>
2023-02-24 06:16:43 -0500

    Add files via upload

---
+++ djsLinkedList/djsLinkedList/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMakeList = New System.Windows.Forms.Button()
+        Me.lstStu = New System.Windows.Forms.ListBox()
+        Me.btnTotal = New System.Windows.Forms.Button()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnMakeList
+        '
+        Me.btnMakeList.Location = New System.Drawing.Point(12, 12)
+        Me.btnMakeList.Name = "btnMakeList"
+        Me.btnMakeList.Size = New System.Drawing.Size(128, 98)
+        Me.btnMakeList.TabIndex = 0
+        Me.btnMakeList.Text = "Make List"
+        Me.btnMakeList.UseVisualStyleBackColor = True
+        '
+        'lstStu
+        '
+        Me.lstStu.FormattingEnabled = True
+        Me.lstStu.Location = New System.Drawing.Point(260, 15)
+        Me.lstStu.Name = "lstStu"
+        Me.lstStu.Size = New System.Drawing.Size(120, 95)
+        Me.lstStu.TabIndex = 1
+        '
+        'btnTotal
+        '
+        Me.btnTotal.Location = New System.Drawing.Point(12, 205)
+        Me.btnTotal.Name = "btnTotal"
+        Me.btnTotal.Size = New System.Drawing.Size(128, 84)
+        Me.btnTotal.TabIndex = 2
+        Me.btnTotal.Text = "Totals"
+        Me.btnTotal.UseVisualStyleBackColor = True
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Location = New System.Drawing.Point(257, 205)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(31, 13)
+        Me.lblTotal.TabIndex = 3
+        Me.lblTotal.Text = "Total"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.btnTotal)
+        Me.Controls.Add(Me.lstStu)
+        Me.Controls.Add(Me.btnMakeList)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnMakeList As Button
+    Friend WithEvents lstStu As ListBox
+    Friend WithEvents btnTotal As Button
+    Friend WithEvents lblTotal As Label
+End Class
---
+++ djsLinkedList/djsLinkedList/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Dim stuName(12) As String
+    Dim qty(12) As Integer
+    Dim link(12) As Integer
+    Dim Current As Integer
+    Dim First As Integer
+    Dim Total As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Total = 0
+        First = 3
+        stuName(1) = ”Barb” : qty(1) = 40 : link(1) = 12
+        stuName(3) = ”Betty” : qty(3) = 20 : link(3) = 5
+        stuName(5) = ”Jim” : qty(5) = 70 : link(5) = 10
+        stuName(6) = ”Jon” : qty(6) = 100 : link(6) = 0
+        stuName(7) = ”Sally” : qty(7) = 60 : link(7) = 1
+        stuName(10) = ”Mike” : qty(10) = 80 : link(10) = 7
+        stuName(12) = ”Julie” : qty(12) = 90 : link(12) = 6
+
+    End Sub
+
+    Private Sub btnMakeList_Click(sender As Object, e As EventArgs) Handles btnMakeList.Click
+        Current = First
+        Do Until (Current = 0)
+            lstStu.Items.Add(stuName(Current))
+            Current = link(Current)
+        Loop
+
+    End Sub
+
+    Private Sub btnTotal_Click(sender As Object, e As EventArgs) Handles btnTotal.Click
+
+
+    End Sub
+End Class
